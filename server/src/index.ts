7ddd8b67a119e95ae53ee1f8780612a46924dbd1
Doan Ngoc Thuong <ngthuongdoan@gmail.com>
2021-06-24 19:12:40 +0700

    refactor: restore normal node server

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import server from "./app"
-require("dotenv").config();
-
-// @ts-ignore
-const PORT: number = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log("Express run on http://localhost:" + PORT)
-})
